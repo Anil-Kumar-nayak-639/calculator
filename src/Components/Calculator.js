517ed5223499b5cc81f11e64d30d647852418fc0
Anil kumar nayak <anilkumarnayak639@gmail.com>
2023-05-19 00:03:48 +0530

    updated

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -3,9 +3,23 @@ import './Calculator.css'
 
 
 const Calculator = () => {
-    const [result, setResult] = React.useState('xyz')
+    const [result, setResult] = React.useState('')
     const click = (e) => {
-        setResult(e.target.value)
+        setResult(result.concat(e.target.name))
+    }
+    const clear = () => {
+        setResult("")
+    }
+    const goback = () => {
+        setResult(result.slice(0, result.length - 1))
+    }
+    const calculate = () => {
+        try {
+            setResult(eval(result).toString())
+        } catch (error) {
+            setResult("ERROR")
+        }
+
     }
     return (
         <div className='x'>
@@ -14,24 +28,24 @@ const Calculator = () => {
                     <input type="text" value={result} />
                 </form>
                 <div className="keypad">
-                    <button className='x'>Clear</button>
-                    <button onClick={click}>C</button>
-                    <button onClick={click}>&divide;</button>
-                    <button onClick={click}>7</button>
-                    <button onClick={click}>8</button>
-                    <button onClick={click}>9</button>
-                    <button onClick={click}>&times;</button>
-                    <button onClick={click}>4</button>
-                    <button onClick={click}>5</button>
-                    <button onClick={click}>6</button>
-                    <button onClick={click}>&ndash;</button>
-                    <button onClick={click}>1</button>
-                    <button onClick={click}>2</button>
-                    <button onClick={click}>3</button>
-                    <button onClick={click}>+</button>
-                    <button onClick={click}>0</button>
-                    <button onClick={click}>.</button>
-                    <button onClick={click}>=</button>
+                    <button className='btn-light' onClick={clear} id='clear'>Clear</button>
+                    <button className='btn-light' onClick={goback}>&lArr;</button>
+                    <button className='btn-light' name='/' onClick={click}>&divide;</button>
+                    <button name='7' onClick={click}>7</button>
+                    <button name='8' onClick={click}>8</button>
+                    <button name='9' onClick={click}>9</button>
+                    <button className='btn-light' name='*' onClick={click}>&times;</button>
+                    <button name='4' onClick={click}>4</button>
+                    <button name='5' onClick={click}>5</button>
+                    <button name='6' onClick={click}>6</button>
+                    <button className='btn-light' name='-' onClick={click}>&ndash;</button>
+                    <button name='1' onClick={click}>1</button>
+                    <button name='2' onClick={click}>2</button>
+                    <button name='3' onClick={click}>3</button>
+                    <button className='btn-light' name='+' onClick={click}>+</button>
+                    <button name='0' onClick={click}>0</button>
+                    <button name='.' onClick={click}>.</button>
+                    <button onClick={calculate} id='equal'>=</button>
                 </div>
             </div>
         </div>
